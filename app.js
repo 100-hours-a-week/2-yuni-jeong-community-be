215c0b4809d96af8a0de8293486897baf70ff6f3
hyoon1129 <hyoon1129@naver.com>
2024-11-10 17:13:12 +0900

    refactor: user, auth 라우트 분리

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import postRoutes from './routes/postRoutes.js';
 import cors from 'cors';
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
 
---
+++ controllers/authController.js
@@ -0,0 +1,66 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const usersFilePath = path.join(__dirname, '../model/users.json');
+
+
+// 회원가입 로직
+export const register = (req, res) => {
+    const { email, password, nickname, profile_image } = req.body;
+
+    // 필수 필드 체크
+    if (!email || !password || !nickname) {
+        return res.status(400).json({ message: "잘못된 요청", data: null });
+    }
+
+    // 사용자 중복 체크
+    fs.readFile(usersFilePath, 'utf8', (err, data) => {
+        if (err) return res.status(500).json({ message: "서버 에러", data: null });
+
+        const users = JSON.parse(data);
+        const existingUser = users.find(user => user.email === email);
+        if (existingUser) {
+            return res.status(409).json({ message: "이미 사용 중인 이메일입니다.", data: null });
+        }
+
+        const newUser = {
+            user_id: users.length + 1,
+            email,
+            password,
+            nickname,
+            profile_image: profile_image || ""
+        };
+        users.push(newUser);
+
+        fs.writeFile(usersFilePath, JSON.stringify(users), 'utf8', (writeErr) => {
+            if (writeErr) return res.status(500).json({ message: "서버 에러", data: null });
+            res.status(201).json({ message: "회원가입 성공", data: { user_id: newUser.user_id } });
+        });
+    });
+};
+
+// 로그인 로직
+export const login = (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: "잘못된 요청", data: null });
+    }
+
+    fs.readFile(usersFilePath, 'utf8', (err, data) => {
+        if (err) return res.status(500).json({ message: "서버 에러", data: null });
+
+        const users = JSON.parse(data);
+        const user = users.find(u => u.email === email && u.password === password);
+
+        if (!user || user.password !== password) {
+            return res.status(400).json({ message: "이메일 또는 비밀번호가 잘못되었습니다.", data: null });
+        }
+        
+
+        res.status(200).json({ message: "login_success", data: { user_id: user.user_id } });
+    });
+};

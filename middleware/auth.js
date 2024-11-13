eb1e213fe6eac159d953c13006d770cb61e62100
hyoon1129 <hyoon1129@naver.com>
2024-11-13 10:56:09 +0900

    feat: 사용자 인증 미들웨어 추가
    - 로그인 되었는지 확인

---
+++ middleware/auth.js
@@ -0,0 +1,7 @@
+export const isAuthenticated = (req, res, next) => {
+    if (req.session.user_id) {
+        next();
+    } else {
+        res.status(401).json({ message: "로그인이 필요합니다." });
+    }
+};

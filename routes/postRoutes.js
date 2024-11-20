3d37bc2e2ff05cdc67cd32d2ce5cd171aef82915
hyoon1129 <hyoon1129@naver.com>
2024-11-20 22:21:05 +0900

    fix: #15 이슈 해결
    - id 생성 방식  UUID로 수정

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,14 +4,28 @@ import upload from '../middleware/upload.js';
 
 const router = express.Router();
 
+// router.get('/page/:page', getAllPosts);
+// router.get('/:postId', getPostById);
+// router.get('/:post_id/comments', getCommentsByPostId);
+// router.post('/', upload.single('image'), createPost);
+// router.post('/:post_id/comments', createComment);
+// router.delete('/:postId', deletePost);
+// router.delete('/:post_id/comments/:comment_id', deleteComment);
+// router.patch('/:postId', upload.single('image'), updatePost);
+// router.patch('/:postId/comments/:commentId', updateComment);
+
+router.get('/:post_id/comments', getCommentsByPostId);
 router.get('/page/:page', getAllPosts);
 router.get('/:postId', getPostById);
-router.get('/:post_id/comments', getCommentsByPostId);
-router.post('/', upload.single('image'), createPost);
+
 router.post('/:post_id/comments', createComment);
-router.delete('/:postId', deletePost);
-router.delete('/:post_id/comments/:comment_id', deleteComment);
-router.patch('/:postId', upload.single('image'), updatePost);
+router.post('/', upload.single('image'), createPost);
+
 router.patch('/:postId/comments/:commentId', updateComment);
+router.patch('/:postId', upload.single('image'), updatePost);
+
+router.delete('/:post_id/comments/:comment_id', deleteComment); 
+router.delete('/:postId', deletePost);
+
 
 export default router;

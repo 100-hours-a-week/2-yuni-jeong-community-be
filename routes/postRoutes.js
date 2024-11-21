b919e84472877cb2a4b7e507fc0a74899cafff71
hyoon1129 <hyoon1129@naver.com>
2024-11-21 10:45:13 +0900

    refactor: 라우터에 isAuthenticated 추가

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -9,15 +9,15 @@ router.get('/:post_id/comments', getCommentsByPostId);
 router.get('/page/:page', getAllPosts);
 router.get('/:post_id', getPostById);
 
-router.post('/:post_id/comments', createComment);
-router.post('/', upload.single('image'), createPost);
+router.post('/:post_id/comments', isAuthenticated, createComment);
+router.post('/', upload.single('image'), isAuthenticated, createPost);
 router.post('/:post_id/like', isAuthenticated, toggleLike);
 
-router.patch('/:post_id/comments/:comment_id', updateComment);
-router.patch('/:post_id', upload.single('image'), updatePost);
+router.patch('/:post_id/comments/:comment_id', isAuthenticated, updateComment);
+router.patch('/:post_id', upload.single('image'), isAuthenticated, updatePost);
 
-router.delete('/:post_id/comments/:comment_id', deleteComment); 
-router.delete('/:post_id', deletePost);
+router.delete('/:post_id/comments/:comment_id', isAuthenticated, deleteComment); 
+router.delete('/:post_id', isAuthenticated, deletePost);
 
 
 export default router;

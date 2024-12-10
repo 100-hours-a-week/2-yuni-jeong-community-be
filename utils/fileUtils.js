8955f9accfb263d9e07aec5ffd734242216e6396
hyoon1129 <hyoon1129@naver.com>
2024-12-10 15:24:17 +0900

    fix: s3 파일 삭제 오류 해결

--- utils/fileUtils.js
+++ utils/fileUtils.js
@@ -1,13 +1,13 @@
 import AWS from 'aws-sdk';
-
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+import { BUCKET_NAME, CLOUDFRONT_URL } from '../utils/constants.js';
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
+const s3 = new S3Client({
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
     region: process.env.AWS_REGION,
 });
-
-const BUCKET_NAME = 'yuni-community-bucket';
-const CLOUDFRONT_URL = 'https://dyto0gfaepgcj.cloudfront.net';
 
 export const uploadToS3 = async (buffer, fileName, contentType) => {
     const params = {
@@ -18,8 +18,8 @@ export const uploadToS3 = async (buffer, fileName, contentType) => {
     };
 
     try {
-        await s3.upload(params).promise();
-        console.log("s3에 파일 업로드 성공")
+        await s3.send(new PutObjectCommand(params));
+        console.log("s3에 파일 업로드 성공");
         return `${CLOUDFRONT_URL}/${fileName}`;
     } catch (error) {
         console.error('파일 업로드 오류 :', error);
@@ -30,11 +30,11 @@ export const uploadToS3 = async (buffer, fileName, contentType) => {
 export const deleteFromS3 = async (fileName) => {
     const params = {
         Bucket: BUCKET_NAME,
-        key: fileName,
+        Key: fileName,
     };
 
     try {
-        await s3.deleteObject(params).promise();
+        await s3.send(new DeleteObjectCommand(params));
         console.log("s3 파일 삭제 성공");
     } catch (error) {
         console.error('파일 삭제 오류 : ', error);

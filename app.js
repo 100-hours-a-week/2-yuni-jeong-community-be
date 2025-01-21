e313955d52c6691adad2c0fd1b8cc99646aa430e
hyoon1129 <hyoon1129@naver.com>
2025-01-21 17:19:35 +0900

    refactor: 세션 설정

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.use(session({
     saveUninitialized: false,
     cookie: { 
         httpOnly: true,
-        secure: false,
-        sameSite: 'lax',
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 24 * 60 * 60 * 1000,
     }
 }));
